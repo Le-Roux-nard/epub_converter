7182b06cab6252251d2bd55817f65499acee3657
Le_Roux-nard <65075968+Le-Roux-nard@users.noreply.github.com>
2025-09-01 12:26:35 +0200

    v1 🎉

---
+++ epub-server.py
@@ -0,0 +1,162 @@
+import base64
+import binascii
+import io
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, request, send_from_directory, abort, send_file, render_template
+import math
+from urllib.parse import unquote
+from typing import List
+from datetime import datetime
+import json
+import mkepub
+import requests, PIL
+import re
+from natsort import natsorted
+
+url_turbo_regex = r"^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"
+image_data_url_regexp = re.compile(r"^data:(image/[\w.+-]+)?;base64,(.*)$", re.IGNORECASE | re.DOTALL)
+
+class NovelMetadata:
+    author:str
+    collection:List[mkepub.BookCollectionMetadata]
+    translator:str
+    synopsys:str
+    cover:str
+    volumeName:str
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return None
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
+def decode_data_url_to_bytes(data_url: str) -> bytes:
+    m = image_data_url_regexp.match(data_url.strip())
+    if not m:
+        raise ValueError("Format data URL invalide. Attendu: data:image/<type>;base64,<...>")
+
+    b64_part = m.group(2)
+    try:
+        return base64.b64decode(b64_part, validate=True)
+    except binascii.Error as e:
+        raise ValueError(f"Contenu base64 invalide: {e}")
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/', methods=["GET", "HEAD"], defaults={'req_path': ''})
+@app.route('/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = 'C:/Users/guyma/Desktop/epub-downloader/result'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, unquote(req_path))
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        if request.method == "GET":
+            return send_file(abs_path)
+        else:
+            return "", 302
+    
+    if request.method != "GET":
+        return "", 405
+    
+    actual_dir = req_path.split("/")[-1]
+    previous_dir = "/".join(req_path.split("/")[:-1])
+
+    # Show directory contents
+    files = list(map(lambda node: {
+        "name": node,
+        "path": f"{actual_dir}/{node}",
+        "isDir": os.path.isdir(os.path.join(abs_path, node)),
+        "size": convert_size(os.path.getsize(os.path.join(abs_path, node))),
+        "mdate": str(datetime.fromtimestamp(os.path.getmtime(os.path.join(abs_path, node))))[:19]
+    }, os.listdir(abs_path)))
+
+
+    return render_template('index.html',previous_dir=previous_dir, actual_dir=req_path, files=natsorted(files, key=lambda f: f["name"]))
+
+
+@app.post('/')
+def buildEpub():
+    print(request.files)
+    files = list(request.files.values())
+
+    htmlFiles = list(filter(lambda f: f.filename == "chapter.html", files))
+    jsonFiles = list(filter(lambda f: f.filename == "metadata.json", files))
+    images = list(filter(lambda f: f.filename.endswith(".png"), files))
+
+    print(files)
+    
+    if len(htmlFiles) != 1 or len(jsonFiles) != 1:
+        return "Can't process files without exactly one 'chapter.html' and one 'metadata.json'",422
+
+    chapterContentFile = htmlFiles[0]
+    metadataFile = jsonFiles[0]
+    
+    if chapterContentFile.content_type != "text/html":
+        return abort(406)
+    
+    if metadataFile.content_type != "application/json":
+        return abort(406)
+
+    for image in images:
+        if image.content_type != "image/png":
+            return abort(406)
+        
+    chapterContentFileStream = chapterContentFile.stream
+    chapterContent = chapterContentFileStream.read().decode()
+    chapterContentFileStream.close()
+    
+    metadataFileStream = metadataFile.stream
+    metadataContent = metadataFileStream.read()
+    metadataFileStream.close()
+
+    try:
+        metadata : mkepub.BookMetadata = json.loads(metadataContent)
+    except:
+        return abort(406)
+
+    epubVolume = mkepub.Book(**metadata)
+
+    if re.match(url_turbo_regex, metadata["cover"]):
+        cover_content = requests.get(metadata["cover"]).content
+        PIL.Image.open(io.BytesIO(cover_content)).verify()
+        epubVolume.set_cover(cover_content)
+    elif re.match(image_data_url_regexp, metadata["cover"]):
+        epubVolume.set_cover(decode_data_url_to_bytes(metadata["cover"]))
+    else:
+        epubVolume.generate_cover()
+
+    epubVolume.add_page(metadata["title"], chapterContent)
+    for imageFile in images:
+        imageFileStream = imageFile.stream
+        imageContent = imageFileStream.read()
+        imageFileStream.close()
+        print(f"adding image {image.filename}")
+        epubVolume.add_image(image.filename, imageContent)
+    
+    file_location = f"./result/{secure_filename(metadata["collections"][0]['name'])}/{secure_filename(metadata["volumeName"])}"
+    file_path = f"{file_location}/{metadata["title"]}.epub"
+
+    os.makedirs(file_location, exist_ok=True)
+    print(f"Saving epub file")
+    epubVolume.save(filename=file_path)
+
+    return "", 202
+
+app.run()
---
+++ tamperMonkey.js
@@ -0,0 +1,454 @@
+// ==UserScript==
+// @name         Dump Chapters
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-29
+// @description  try to take over the world!
+// @author       You
+// @match        https://victorian-novel-house.fr/lecture/*/volumes/*/chapitres/*
+// @match        https://victorian-novel-house.fr/oeuvres/*
+// @match        https://world-novel.fr/lecture/*/volumes/*/chapitres/*
+// @match        https://world-novel.fr/oeuvres/*
+// @match        https://cdn.victorian-novel-house.fr/images/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=victorian-novel-house.fr
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.listValues
+// @grant        GM.removeValues
+// ==/UserScript==
+
+const backupServerURL = "http://localhost:5000";
+
+const lineBreakStyle = `
+<style>
+  p {
+		margin: 13px 0;
+		/* display:block; */
+	}
+
+	body, div, img {
+		padding: 0pt;
+		margin: 0pt;
+		line-height: 125%;
+	}
+
+	body {
+		text-align: left;
+	}
+
+	.line-break {
+		display: block;
+		width: 100%;
+		height: 1em;
+		position:relative
+	}
+
+	.line-break::after {
+		position: absolute;
+		font-style:italic;
+		content: "* * *";
+		left: 50%;
+		transform: translateX(-75%);
+	}
+
+
+	img {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 1em;
+	max-width: 75%;
+	max-height: 80%;
+	object-fit: contain;
+	} 
+</style>
+`;
+
+function wait(time) {
+	return new Promise((res) => setTimeout(res, time));
+}
+
+function waitForElement(selector) {
+	return new Promise((res) => {
+		const observer = new MutationObserver((mutations, observer) => {
+			const element = document.querySelector(selector);
+			if (element) {
+				observer.disconnect();
+				res();
+			}
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
+		});
+	});
+}
+
+function waitForChapterLoad(selector) {
+	return new Promise((res) => {
+		const observer = new MutationObserver(() => {
+			observer.disconnect();
+			res();
+		});
+
+		// call `observe()`, passing it the element to observe, and the options object
+		observer.observe(document.querySelector(selector), {
+			subtree: true,
+			characterData: true,
+		});
+
+		observer.observe(document.querySelector(selector), {
+			subtree: true,
+			childList: true,
+		});
+	});
+}
+
+function waitForTabToBeClosed(tab) {
+	return new Promise((res) =>
+		setInterval(() => {
+			if (tab.closed) res();
+		}, 500)
+	);
+}
+
+async function loadCSSCors(stylesheet_uri) {
+	return new Promise((res) => {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", stylesheet_uri);
+		xhr.onload = function () {
+			xhr.onload = xhr.onerror = null;
+			if (xhr.status < 200 || xhr.status >= 300) {
+				alert("style failed to load: " + stylesheet_uri);
+			} else {
+				var style_tag = document.createElement("style");
+				style_tag.appendChild(document.createTextNode(xhr.responseText));
+				document.head.appendChild(style_tag);
+				res();
+			}
+		};
+		xhr.onerror = function () {
+			xhr.onload = xhr.onerror = null;
+			alert("XHR CORS CSS fail:" + stylesheet_uri);
+		};
+		xhr.send();
+	});
+}
+
+async function waitForLocalStorage(key, interval = 100) {
+	return new Promise((res) => {
+		let checker = setInterval(async () => {
+			const keys = await GM.listValues();
+			if (keys.includes(key)) {
+				clearInterval(checker);
+				const value = GM.getValue(key);
+				GM.deleteValue(key);
+				return res(value);
+			}
+		}, interval);
+	});
+}
+
+function openInNewTab(url) {
+	const a = document.createElement("a");
+	a.href = url;
+	a.target = "_blank";
+	a.rel = "noopener noreferrer";
+	a.referrerPolicy = "no-referrer";
+	document.body.appendChild(a);
+	a.click();
+	a.remove();
+}
+
+async function getImageData(url) {
+	openInNewTab(url);
+	return await waitForLocalStorage(url.split("/").at(-1));
+}
+
+async function getNovelMetadata() {
+	const novelTitleNode = document.querySelector("h2");
+	const synopsisParagraph = document.querySelector("section > div > div > p");
+	const authorSpan = document.querySelector("section > div span:nth-child(1)");
+	const translatorSpan = document.querySelector("section > div span:nth-child(2)");
+	const coverElement = document.querySelector("section div img");
+	const coverDataURL = await getImageData(coverElement.src);
+
+	return {
+		author: authorSpan.innerText.split(":").at(-1).trim(),
+		collection: {
+			id: window.location.pathname.split("/").at(-1),
+			name: novelTitleNode.innerText,
+		},
+		translator: translatorSpan.innerText.split(":").at(-1).trim(),
+		synopsys: synopsisParagraph.innerText,
+		cover: coverDataURL,
+	};
+}
+
+async function getVolumeMetada(novelMetadata, volumeName) {
+	let volumeNumber;
+	[volumeName, volumeNumber] = /(?<=(\d+) - ).+|^.+ (\d+)$/.exec(volumeName).filter((value) => !!value) ?? [volumeName, 0];
+
+	// const getApiBookSearchURL = (author, novelName, bookName) => `https://www.googleapis.com/books/v1/volumes?q=inauthor:${author},intitle:${encodeURIComponent(novelName)},intitle:${encodeURIComponent(bookName)}`;
+	const getApiBookSearchURL = (author, novelName, bookName) => `https://www.googleapis.com/books/v1/volumes?q=${author},${encodeURIComponent(novelName)},${encodeURIComponent(bookName)}`;
+
+	const verifyResultsItemsFunction = (item, bookName) => item.volumeInfo.title.includes(bookName) || item.volumeInfo?.subtitle?.includes(bookName);
+
+	novelCollection = JSON.parse(JSON.stringify(novelMetadata.collection));
+	novelCollection.number = volumeNumber;
+
+	let bookSearchResults;
+	let availableTitleVariation = [volumeName, `Vol ${volumeNumber}`, `Vol. ${volumeNumber}`, `Book ${volumeNumber}`];
+
+	bookSearchResults = await fetch(getApiBookSearchURL(novelMetadata.author, novelMetadata.collection.name, volumeName)).then((r) => r.json());
+
+	if (bookSearchResults.totalItems === 0) {
+		//No match found
+		return {
+			...novelMetadata,
+			collection: novelCollection,
+			volumeName: volumeName,
+		};
+	} else {
+		let validBook;
+		for (const variation of availableTitleVariation) {
+			validBook = bookSearchResults.items.find((i) => verifyResultsItemsFunction(i, variation));
+			if (!!validBook) break;
+		}
+		if (!validBook) {
+			return {
+				...novelMetadata,
+				collection: novelCollection,
+				volumeName: volumeName,
+			};
+		}
+		let bookName = `${validBook.volumeInfo.title} ${validBook.volumeInfo.subtitle ?? ""}`;
+
+		console.log("-".repeat(50))
+		console.log(bookName)
+
+		availableTitleVariation.shift() // remove the book title from the loop to avoid it being replaced
+
+		for (variation of availableTitleVariation) {
+			bookName = bookName.replace(variation, "");
+			console.log(bookName)
+		}
+
+
+		bookName = bookName
+			.replace(novelMetadata.collection.name, "")
+			.replace(/^[^A-Za-z0-9]+|[^A-Za-z0-9]+$/g, "")
+			.trim();
+
+		console.log(bookName, novelMetadata.collection.name)
+		
+		if (bookName == "") {
+			bookName = validBook.volumeInfo.title;
+		}
+		
+		console.log(bookName)
+
+
+		let volumeCover;
+		if (!!validBook.volumeInfo?.imageLinks?.thumbnail) {
+			volumeCover = `https://books.google.com/books/publisher/content/images/frontcover/${validBook.id}?fife=w10000`;
+		}
+
+		return {
+			...novelMetadata,
+			collection: novelCollection,
+			volumeName: volumeName,
+			synopsys: validBook.volumeInfo.description,
+			cover: volumeCover ?? novelMetadata.cover,
+		};
+	}
+}
+
+(async function () {
+	"use strict";
+
+	const params = window.location.search;
+	const pathname = document.location.pathname;
+
+	if (pathname.match(/\/images\/.+?\/.+/)) {
+		//#region Gestion des Images
+		const canvas = document.createElement("canvas");
+		const ctx = canvas.getContext("2d");
+		const img = document.querySelector("img");
+
+		canvas.width = img.width;
+		canvas.height = img.height;
+
+		ctx.drawImage(img, 0, 0, img.width, img.height);
+
+		const localStorageKey = pathname.split("/").at(-1);
+		await GM.setValue(localStorageKey, canvas.toDataURL("image/png"));
+		return window.close();
+		//#endregion
+	}
+
+	if (pathname.match(/\/oeuvres\/([a-z\-0-9]+)/)) {
+		//#region Oeuvre
+
+		//unfold all volumes to load chapters in the DOM
+		await waitForElement("div > h3");
+		const volumes = [...document.querySelectorAll("div > h3")];
+		const novelMetadata = await getNovelMetadata();
+		await window.localStorage.setItem(`${novelMetadata.collection.id}`, JSON.stringify(novelMetadata));
+
+		volumes.reverse();
+
+		const localVolumesMetadata = {};
+		for await (const volume of volumes) {
+			const volumeMetadata = await getVolumeMetada(novelMetadata, volume.innerText);
+			localVolumesMetadata[volume.innerText] = volumeMetadata;
+			window.localStorage.setItem(`${volumeMetadata.collection.id}-${volumeMetadata.collection.number}`, JSON.stringify(volumeMetadata));
+		}
+
+		if (!params.includes("dump=true")) return;
+
+		for await (const volume of volumes) {
+			volume.click();
+			let volumeMetadata = localVolumesMetadata[volume.innerText];
+			await waitForElement("div > ul a");
+			const chapters = [...volume.nextSibling.querySelectorAll("a")];
+			for await (const chapter of chapters.reverse()) {
+				let tab = window.open(`${chapter.href}?dump=true&close=true`, "_blank");
+				await waitForTabToBeClosed(tab);
+			}
+			volume.click();
+		}
+
+		return;
+		//#endregion
+	} else if (pathname.match(/\/lecture\/(['a-z\-0-9]+)\/volumes\/.+\/chapitres\/(.+)/)) {
+		if (!params.includes("dump=true")) return;
+
+		//#region Chapitre
+		const chapterContentLocator = "#textContainer > .chapter-obf";
+
+		await waitForElement(chapterContentLocator);
+
+		try {
+			const cssSheet = [...document.querySelectorAll("link[rel=stylesheet]")].find((s) => s.href.includes("chapitre"));
+			await loadCSSCors(cssSheet.href);
+
+			for (const styleSheet of document.styleSheets) {
+				try {
+					for (const rule of styleSheet.cssRules) {
+						if (!!rule && !!rule.style && rule.style["font-size"] === "0px") {
+							document.querySelectorAll(rule.selectorText).forEach((e) => e.remove());
+						}
+					}
+				} catch (err) {}
+			}
+
+			const chapterContainer = document.querySelector(chapterContentLocator);
+			let chapterContent = chapterContainer.innerHTML;
+
+			chapterContent = chapterContent.replace(/<span class=".{8}">|<\/span>/g, "");
+			chapterContent = chapterContent.replace(/(?:<br\s*\/*>)+/, "\n");
+
+			const chapterImages = {};
+			for await (const image of [...chapterContainer.querySelectorAll("img")]) {
+				let newImageName = image.alt.replace(/\..+?$/, ".png");
+				if (newImageName in chapterImages) return alert("Image ALT already used ! something might be wrong");
+				const imageDataURL = await getImageData(image.src);
+				chapterImages[newImageName] = imageDataURL;
+				chapterContent = chapterContent.replace(image.src, `images/${newImageName}`).replace(image.alt, "");
+			}
+
+			chapterContent = `${lineBreakStyle}\n${chapterContent}`; //ajout de la feuille de style custom en premier
+
+			// Remplace ceci par l'URL de ton webhook Discord
+			const formData = new FormData();
+
+			let imageKeys = Object.keys(chapterImages);
+			let i = 0;
+			for (i in imageKeys) {
+				let imageName = imageKeys[i];
+				let pictureDataURL = chapterImages[imageName];
+				const byteString = atob(pictureDataURL.split(",")[1]); // Décodage de la partie base64
+				const arrayBuffer = new ArrayBuffer(byteString.length);
+				const uintArray = new Uint8Array(arrayBuffer);
+
+				for (let j = 0; j < byteString.length; j++) {
+					uintArray[j] = byteString.charCodeAt(j);
+				}
+
+				const blob = new Blob([uintArray], { type: "image/png" }); // Change le type en fonction du format
+				const file = new File([blob], imageName, { type: "image/png" });
+
+				formData.append(`files[${i}]`, file);
+			}
+
+			const uint8ArrayChapterContent = new TextEncoder().encode(chapterContent);
+			const chapterContentBlob = new Blob([uint8ArrayChapterContent], { type: "text/html" });
+			const chapterFile = new File([chapterContentBlob], `chapter.html`, { type: "text/html" });
+
+			i += 1; // incrémente pour intégrer le chapitre
+
+			formData.append(`files[${i}]`, chapterFile);
+
+			const novelName = document
+				.querySelector("main div div a:nth-child(2)")
+				.innerText.toLowerCase()
+				.replace(/[^'a-z]/g, "-");
+
+			const chapterName = document.querySelector("main div div span:nth-of-type(2)").innerText;
+			const volumeNumber = document.querySelector("main div div span").innerText.replace(/[^0-9]/g, "") ?? "0";
+			const localStorageMetadata = JSON.parse(await window.localStorage.getItem(`${novelName}-${volumeNumber}`));
+			if (!localStorageMetadata) return alert("Impossible d'extraire ce chapitre, merci de charger la page de l'oeuvre dans un premier temps");
+
+			const metadata = {
+				title: chapterName,
+				lang: "fr",
+				collections: [
+					{
+						...localStorageMetadata.collection,
+						type: "series",
+					},
+				],
+				creators: [
+					{
+						name: localStorageMetadata.author,
+						role: "aut",
+					},
+				],
+				contributors: [
+					{
+						name: localStorageMetadata.translator,
+						role: "trl",
+					},
+				],
+				subjects: [],
+				description: localStorageMetadata.synopsys,
+				cover: localStorageMetadata.cover,
+				volumeName: localStorageMetadata.volumeName,
+			};
+
+			const uint8ArrayMetadata = new TextEncoder().encode(JSON.stringify(metadata, null, 4));
+			const metadataBlob = new Blob([uint8ArrayMetadata], { type: "application/json" });
+			const metadataFile = new File([metadataBlob], `metadata.json`, { type: "application/json" });
+			i += 1; // incrémente pour intégrer les métadonnées du chapitre
+
+			formData.append(`files[${i}]`, metadataFile);
+
+			try {
+				await fetch(backupServerURL, {
+					method: "POST",
+					body: formData,
+				});
+			} catch (error) {
+				console.error("❌ Erreur lors de l'envoi :", error);
+			}
+
+			if (params.includes("close=true")) {
+				return window.close();
+			}
+		} catch (err) {
+			alert(err);
+		}
+		//#endregion
+	}
+})();
